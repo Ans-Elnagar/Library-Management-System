c65735b0110159dbf97bf603bf8495b451b95af3
Ans-Elnagar <ansgom3a@gmail.com>
2024-08-12 19:00:11 +0300

    Creating the project

---
+++ src/test/java/cc/maids/test/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.maids.test.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
